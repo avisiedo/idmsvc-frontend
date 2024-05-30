d6c5de93243dd5799010a35db4911117ff70566e
Alejandro Visiedo <avisiedo@redhat.com>
2024-05-30 11:24:56 +0200

    wip feat: scaffolding helper
    Add initial helper scripts for scaffolding page
    and components.
    
    Signed-off-by: Alejandro Visiedo <avisiedo@redhat.com>

---
+++ scripts/sh/new-component.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+# This script generate a skeleton for a new page
+
+DEFAULT_PAGE_COMPONENT="MyNewRoute"
+
+PAGE_COMPONENT="${PAGE_COMPONENT:-${DEFAULT_PAGE_COMPONENT}}"
+
+function prompt_page_component {
+    printf "Component Name for your new page? [${DEFAULT_PAGE_COMPONENT}] "
+    read PAGE_COMPONENT
+    [ "${PAGE_COMPONENT}" == "" ] || PAGE_COMPONENT="${DEFAULT_PAGE_COMPONENT}"
+    validate_page_component "${PAGE_COMPONENT}" || error "invalid PAGE_COMPONENT value"
+}
+
+function generate_page_component {
+    [ -e "src/Routes/${PAGE_COMPONENT}" ] || mkdir -p "src/Routes/${PAGE_COMPONENT}" || error "creating directory 'src/Routes/${PAGE_COMPONENT}'"
+
+    ## Add the main component skeleton
+    cat > "src/Routes/${PAGE_COMPONENT}/${PAGE_COMPONENT}.tsx" << EOF
+import { useNavigate } from 'react-router-dom';
+import React, { useContext, useEffect, useState } from 'react';
+
+const ${PAGE_COMPONENT} = () => {
+    // TODO Add here your states
+    const [myState, setMyState] = useState<string || undefined>();
+
+
+    // TODO Add here your side effects if any
+    // 
+
+    // TODO Add here your callbacks
+    // 
+
+    // TODO Update your page render
+    // https://www.patternfly.org/components/all-components
+    return (
+        <>
+        </>
+    );
+};
+
+export default ${PAGE_COMPONENT};
+EOF
+
+    ## Add an empty style file
+    cat > "src/Routes/${PAGE_COMPONENT}/${PAGE_COMPONENT}.scss" <<EOF
+@import '~@redhat-cloud-services/frontend-components-utilities/styles/variables';
+EOF
+
+    ## Add the unit test file
+    cat > "src/Routes/${PAGE_COMPONENT}/${PAGE_COMPONENT}.test.tsx" <<EOF
+@import '~@redhat-cloud-services/frontend-components-utilities/styles/variables';
+EOF
+
+    return 0
+}
+
+prompt_page_component
+
+generate_page_component "${PAGE_COMPONENT}"
